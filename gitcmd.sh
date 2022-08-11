14337b3ee68f685fd742d0a9d7fed05173a5e6ee
he_pron <813662830@qq.com>
2022-08-11 21:11:31 +0800

    temp

--- gitcmd.sh
+++ gitcmd.sh
@@ -4,7 +4,7 @@ git init
 #  手动创建.gitignore文件 声明哪些文件(夹)不许要添加到版本控制
 
 #  关联远程源(远程仓库最好空空如也)
-git remote add origin https://gitee.com/he-pron/JS.git
+git remote add origin https://github.com/Cinematique/JS.git
 git remote -v
 
 git add .
